a485b2148624565f8e7448f9140cf736df54bd48
Godswill David <davidgodswill@gmail.com>
2025-01-29 20:28:41 +0100

    - added auth routes with reg and login

--- models/task.py
+++ models/task.py
@@ -4,6 +4,8 @@ from sqlalchemy.orm import relationship
 from db.base_model import Base
 import enum
 
+from models.user import User
+
 
 class TaskPriority(str, enum.Enum):
     LOW = "low"
@@ -22,11 +24,10 @@ class Task(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     description = Column(String)
-    # owner_id = Column(Integer, ForeignKey("users.id"))
     status = Column(Enum(TaskStatus), default=TaskStatus.TODO)
     priority = Column(Enum(TaskPriority), default=TaskPriority.MEDIUM)
     due_date = Column(DateTime(timezone=True), nullable=True)
-    user_id = Column(Integer, ForeignKey("users.id"))
+    user_id = Column(Integer, ForeignKey(User.id, ondelete="CASCADE"))  # Correct FK reference
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
 
